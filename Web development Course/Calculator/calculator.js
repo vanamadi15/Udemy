66e4bc1274e53332730f2d218dc66b71d3918513
vanamadi15 <vsp2744330@gmail.com>
2023-07-05 10:52:28 +0530

    BMI Calculator

--- Web development Course/Calculator/calculator.js
+++ Web development Course/Calculator/calculator.js
@@ -9,13 +9,31 @@ app.get("/", function(req, res){
 })
 
 app.post("/", function(req, res){
-      var num1 = req.body.num1;
-      var num2 = req.body.nun2;
+      var num1 = Number(req.body.num1);
+      var num2 = Number(req.body.nun2);
       var result = num1 + num2;
 
       // console.log("Result : " + result);
       res.send("Result : " + result)
 })
+
+// Caculate BMI
+app.get("/bmicalculator", function (req, res) {
+      res.sendFile(__dirname+"/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function(req, res){
+      var weight = parseFloat(req.body.Weight);
+      var height = parseFloat(req.body.height);
+      // height = Math.pow(height, 2);
+
+      var result = weight / height;
+
+      // console.log("Result : " + result);
+      res.send("Your BMI is : " + result)
+})
+
+
 
 app.listen(3000, function(){
       console.log("Listening to 3000 port");
