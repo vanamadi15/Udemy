f75eaa387e0ac5bb896af0c79dbd161c3c909fd4
vanamadi15 <vsp2744330@gmail.com>
2023-07-08 19:44:14 +0530

    Weather web application using external weather api

--- Web development Course/WeatherProject/app.js
+++ Web development Course/WeatherProject/app.js
@@ -1,35 +1,42 @@
 const { log } = require("console");
 const express = require("express");
 const https = require("https");
+const Bodyparser = require("body-parser");
 const { url } = require("inspector");
 
 const app = express();
-
+app.use(Bodyparser.urlencoded({extended:true}));
 app.get("/", function(req, res){
+      // res.send("Hello weather application");
+      res.sendFile(__dirname+"/index.html");
+});
 
-      const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=899553f5db7d1c1cde59f95d17b09be7&units=metric";
+app.post("/", function(req, res) {
+
+      
+      const query = req.body.cityName;
+      //console.log(query);
+      const apiKey = "899553f5db7d1c1cde59f95d17b09be7";
+      const metric = "metric";
+      const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+metric+".";
 
       https.get(url, function (response) {
             console.log(response.statusCode);
 
             response.on("data", function (data) {
                   const weatherData = JSON.parse(data);
-                  const temp = weatherData.main.temp ;
+                  const temperature = weatherData.main.temp;
                   const weatherDescription = weatherData.weather[0].description ;
-                  const imageUrl = "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon +"@2x.png"
+                  const icon = weatherData.weather[0].icon;
+                  const imageUrl = "http://openweathermap.org/img/wn/"+icon +"@2x.png"
                   // const weatherIcon = ("<img src=" + imageUrl+">")
-                  res.write("Weather condition is : "+ weatherDescription)
-                  res.write("the temperature in london is "+ temp)
-                  res.write("<img src="+imageUrl+">")
+                  res.write("<p>Weather condition is : "+ weatherDescription+ "</p><br>");
+                  res.write("<h1>The temperature in "+query+" is "+ temperature+"</h1>");
+                  res.write("<img src= "+ imageUrl +" >");
 
                   res.send()
             })
       })
-
-      
-
-      // res.send("Hello weather application");
-
 })
 
 
